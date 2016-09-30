3c557b9a605541108cf130a36aa455ee7efe2b92
pjkohler <pjkohler@stanford.edu>
2016-09-30 14:12:21 -0700

    added option to average trials within subject, or not

--- functions/aggregateData.m
+++ functions/aggregateData.m
@@ -1,4 +1,4 @@
-function [avgData] = aggregateData(rcaData,rcaSettings,keepConditions,ampErrorType)
+function [avgData,realSubjs,imagSubjs] = aggregateData(rcaData,rcaSettings,keepConditions,ampErrorType,trialError)
 % [avgData] = aggregateBins(rcaData,rcaSettings,[keepConditions],[ampErrorType])
 %
 % rcaData: created during call to rcaSweep
@@ -25,12 +25,15 @@ function [avgData] = aggregateData(rcaData,rcaSettings,keepConditions,ampErrorTy
 if nargin<2, error('You must provide the rcaSettings struct created when your rca data were created.'); end
 if nargin<3, keepConditions=false; end
 if (nargin<4 || isempty(ampErrorType)), calcErrors=false; else calcErrors = true; end
+if (nargin<5 || isempty(trialError)), trialError=false; else end
+
 
 nConditions = size(rcaData,1);
 nSubjects = size(rcaData,2);
 nFreqs = length(rcaSettings.freqsToUse);
 nBins = length(rcaSettings.binsToUse);
-nCompFromInputData = size(rcaData{1,1},2);
+nCompFromInputData = max(max(cellfun(@(x) size(x,2),rcaData)));
+nTrials = max(max(cellfun(@(x) size(x,3),rcaData)));
 
 % convert to real/imaginary
 [rcaDataReal,rcaDataImag] = getRealImag(rcaData);
@@ -57,21 +60,35 @@ if keepConditions
         end     
         
         if calcErrors
-            % grab all subjects' data, averaging over trials: 
-            muRcaDataRealAllSubj = nan(nBins*nFreqs,nCompFromInputData,nConditions);
-            muRcaDataImagAllSubj = nan(nBins*nFreqs,nCompFromInputData,nConditions);
-            for s=1:nSubjects
-                muRcaDataRealAllSubj(:,:,s) = nanmean(rcaDataReal{condNum,s},3);
-                muRcaDataImagAllSubj(:,:,s) = nanmean(rcaDataImag{condNum,s},3);
+            if trialError
+                % grab all subjects' data, averaging over trials: 
+                tempReal = [];
+                tempImag = [];
+                for s=1:nSubjects
+                    tempReal = cat(3,tempReal,rcaDataReal{condNum,s});
+                    tempImag = cat(3,tempImag,rcaDataImag{condNum,s});
+                end
+                muRcaDataRealAllSubj(:,:,:,condNum) = nan(nBins*nFreqs,nCompFromInputData,nSubjects*nTrials);
+                muRcaDataImagAllSubj(:,:,:,condNum) = nan(nBins*nFreqs,nCompFromInputData,nSubjects*nTrials);
+                muRcaDataRealAllSubj(:,:,1:size(tempReal,3),condNum) = tempReal; % 1:size(tempReal,3) - make it possible to have subjects with fewer trials
+                muRcaDataImagAllSubj(:,:,1:size(tempImag,3),condNum) = tempImag;
+            else
+                % grab all subjects' data, averaging over trials: 
+                muRcaDataRealAllSubj(:,:,:,condNum) = nan(nBins*nFreqs,nCompFromInputData,nSubjects);
+                muRcaDataImagAllSubj(:,:,:,condNum) = nan(nBins*nFreqs,nCompFromInputData,nSubjects);
+                for s=1:nSubjects
+                    muRcaDataRealAllSubj(:,:,s,condNum) = nanmean(rcaDataReal{condNum,s},3);
+                    muRcaDataImagAllSubj(:,:,s,condNum) = nanmean(rcaDataImag{condNum,s},3);
+                end
             end
             
             for rc=1:nCompFromInputData
                 for f=1:nFreqs
                     for b=1:nBins
                         firstDimInd = rcaSettings.freqIndices==rcaSettings.freqsToUse(f) & rcaSettings.binIndices==rcaSettings.binsToUse(b);
-                        realSubjs = squeeze(muRcaDataRealAllSubj(firstDimInd,rc,:));
-                        imagSubjs = squeeze(muRcaDataImagAllSubj(firstDimInd,rc,:));
-                        xyData = [realSubjs imagSubjs];
+                        realSubjs(b,:,f,rc,condNum) = squeeze(muRcaDataRealAllSubj(firstDimInd,rc,:,condNum));
+                        imagSubjs(b,:,f,rc,condNum) = squeeze(muRcaDataImagAllSubj(firstDimInd,rc,:,condNum));
+                        xyData = [realSubjs(b,:,f,rc,condNum)' imagSubjs(b,:,f,rc,condNum)'];
                         if size(xyData,1)<2
                             keyboard;
                         end
@@ -96,24 +113,24 @@ else
         end
     end
     
-    if calcErrors
-        withinSubj = false;
-        ampErrBins = nan(nBins,nFreqs,nCompFromInputData,2);
-        % grab all subjects' data, averaging over trials:
-        muRcaDataRealAllSubj = nanmean(cat(4,rcaDataReal{:}),3);
-        muRcaDataImagAllSubj = nanmean(cat(4,rcaDataImag{:}),3);
-        for rc=1:nCompFromInputData
-            for f=1:nFreqs
-                for b=1:nBins
-                    firstDimInd = rcaSettings.freqIndices==rcaSettings.freqsToUse(f) & rcaSettings.binIndices==rcaSettings.binsToUse(b);
-                    realSubjs = squeeze(muRcaDataRealAllSubj(firstDimInd,rc,:));
-                    imagSubjs = squeeze(muRcaDataImagAllSubj(firstDimInd,rc,:));
-                    xyData = [realSubjs imagSubjs];
-                    [~,ampErrBins(b,f,rc,:)] = getErrorEllipse(xyData,withinSubj,ampErrorType);
-                end
-            end
-        end
-    end
+    %     if calcErrors
+    %         withinSubj = false;
+    %         ampErrBins = nan(nBins,nFreqs,nCompFromInputData,2);
+    %         % grab all subjects' data, averaging over trials:
+    %         muRcaDataRealAllSubj = nanmean(cat(4,rcaDataReal{:}),3);
+    %         muRcaDataImagAllSubj = nanmean(cat(4,rcaDataImag{:}),3);
+    %         for rc=1:nCompFromInputData
+    %             for f=1:nFreqs
+    %                 for b=1:nBins
+    %                     firstDimInd = rcaSettings.freqIndices==rcaSettings.freqsToUse(f) & rcaSettings.binIndices==rcaSettings.binsToUse(b);
+    %                     realSubjs = squeeze(muRcaDataRealAllSubj(firstDimInd,rc,:));
+    %                     imagSubjs = squeeze(muRcaDataImagAllSubj(firstDimInd,rc,:));
+    %                     xyData = [realSubjs imagSubjs];
+    %                     [~,ampErrBins(b,f,rc,:)] = getErrorEllipse(xyData,withinSubj,ampErrorType);
+    %                 end
+    %             end
+    %         end
+    %     end
 end
 
 ampBins = sqrt(realBins.^2+imagBins.^2);
