c8ea8edf6d529ef57fcb60f0e34072397f0d5bc3
Peter J. Kohler, PhD <pjkohl3r@gmail.com>
2017-02-01 23:24:10 -0800

    Zero2NaN functions (could probably be improved)

---
+++ functions/Zero2NaN.m
@@ -0,0 +1,21 @@
+function mOut = pkZero2NaN(m,dim)
+    mOut = m;
+    for d=1:length(dim)
+        if dim(d)>3
+            error('dimensions above 3 not allowed');
+        else
+        end
+        repDims = [1,1,1];
+        repDims(dim(d)) = size(m,dim(d));
+        nanMat = repmat(nansum(m,dim(d)),repDims);
+        idx=find(nanMat==0);
+        if ~isempty(idx)
+            [k{1},k{2},k{3}] = ind2sub(size(m),idx);
+            for n=1:length(k{1}); 
+                mOut(k{1}(n),k{2}(n),k{3}(n)) = NaN;
+            end
+        else
+        end
+    end
+end
+
